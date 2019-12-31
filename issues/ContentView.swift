dbf85fb2b8ca471bace3284f69d8f04d0346ff3a
MRW Neundorf <ninevillage+naxmefy@gmail.com>
2019-12-31 13:48:05 +0100

    Initial Commit

---
+++ issues/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  issues
+//
+//  Created by MRW Neundorf on 31.12.19.
+//  Copyright © 2019 MRW Neundorf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
